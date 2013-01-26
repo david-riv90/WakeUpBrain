516d7caa61943df83eb2e951a4efdd1af37c04de
David Ríos Valeriano <david.riv90@gmail.com>
2013-01-25 19:24:52 -0600

    Clase principal
    Se agregó la clase principal que contiene los métodos para inicializar
    la aplicación

---
+++ src/vista/Principal.java
@@ -0,0 +1,18 @@
+package vista;
+
+/**
+ * En esta clase se encuentran todos los métodos que permiten la inicialización de la aplicación
+ * @author Davex
+ *
+ */
+public class Principal {
+
+	/**
+	 * El método de entrada al programa
+	 * @param args Contiene argumentos de inicialización
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
